ff652674bc31c75d5bc941bb3034270acc0f4a5b
wanway <wanway0311@gmail.com>
2013-12-19 10:40:02 +0800

    finish the test,include help and link test

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,11 @@
+include ApplicationHelper
 # encoding: utf-8
 # 对每个页面，返回一个完整的标题。
-def full_title(page_title)
-	base_title = "一个简单的微博"
-	if page_title.empty?
-		base_title
-	else
-		"#{page_title} | #{base_title}"
-	end
-end
+# def full_title(page_title)
+#	base_title = "一个简单的微博"
+#	if page_title.empty?
+#		base_title
+#	else
+#		"#{page_title} | #{base_title}"
+#	end
+# end
