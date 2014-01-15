99b0f1c5c882982f48143113e9ea92f7abd86b24
wanway <wanway0311@gmail.com>
2014-01-15 16:43:44 +0800

    move find_content_from from helper to controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,17 +8,18 @@ class MessagesController < ApplicationController
 		# 而私信的字段是 id、content、user_id（发送者的ID）、receive_user_id（接收者的ID）
 		# 所以，我需要从提交过来的混合体中分享出内容和接收者
 
-		# 这个的作用是，找到接收者
+		# 以下这个的作用是，找到接收者
 		# 虽然能大体达到效果，
 		# 		但后面的 Message.new(content: message_params) 是为了构建一个 message 对象而为，没有实际作用
 		# 		需要改正
 		@receive_user = User.find_user_from_message_content(Message.new(content: message_params)
 		
-		# 这个的作用是，找到内容
+		# 以下这个的作用是，找到内容
 		# 虽然能达到效果，
-		#		但这貌似不是对象的写法，需要改正，这个方法定义在 messages_helper.rb 中
+		#		但这貌似不是对象的写法，需要改正，这个方法定义在 messages_helper.rb 中		
 		@content = find_content_from(message_params)
 
+		
 		# 准备完毕，建立一条私信
 		@message = current_user.messages.build(content: @content, receive_user_id: @receive_user.id)
 
@@ -37,6 +38,12 @@ class MessagesController < ApplicationController
 		def message_params
 			params.require(:message).permit(:content)
 		end
+
+		# 从表单发送的私信内容中，分离出真正的内容
+		def find_content_from(content)
+			content =~ /^d (\S+) (\S+)/
+			$2
+		end
 end
 
 # 我的问题是
@@ -44,5 +51,3 @@ end
 # 1、刚才从表单过来的值，我用 User.find_user_from_message_content 是不是有问题啊，怎么样写，会比较正确。
 
 # 2、刚才从表单过来的值，我用 find_content_from(message_params) 这个 help 方法，是不是也是错的，正确的思路是怎么样的
-
-
